9024db45cd9a2a7ddb4a6ee83ab2eb86996f485b
Leon <lhc3538@gmail.com>
2018-09-25 16:05:25 +0800

    change indensity to intensity

--- point_cnn/pointcnn_seg_data_fountain/test_intensity.sh
+++ point_cnn/pointcnn_seg_data_fountain/test_intensity.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=3
 
-python3 ../test_seg_data_fountain_intensity.py \
+python3 ../test_seg_data_fountain_indensity.py \
 -f ../dataset_data_fountain/test/data_fountain_files.txt \
--d ../dataset_data_fountain/test_results/ \
--l ../model_data_fountain/h5/seg/pointcnn_seg_data_fountain_all_intensity/iter-320000 \
+-d ../dataset_data_fountain/download/submit_example/ \
+-l ../model_data_fountain/h5/seg/pointcnn_seg_data_fountain_data_fountain_x4_2048_xyrgbi_3162_2018-09-20-08-58-57/ckpts/iter-94000 \
 -m pointcnn_seg_data_fountain \
--x data_fountain_x4_2048_xyrgbi_intensity
+-x data_fountain_x4_2048_xyrgbi
 
